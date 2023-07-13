24b4f5e4c7badced9175c208fd3e955667b4490f
unknown <raoulkuete74@gmail.com>
2023-07-13 11:32:14 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// const menuBtn = document.querySelector('.menu-btn');
+// const navitems = document.querySelector('.navitems');
+
+// menuBtn.addEventListener('click', () => {
+//   navitems.classList.toggle('active');
+// });
+
